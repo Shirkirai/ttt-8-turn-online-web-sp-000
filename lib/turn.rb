d69cb57f1cbfe214a97fcb5fe320f131faac83f0
Jung Lee <lee.jung.s95@gmail.com>
2020-04-10 03:15:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    return move(board, index, "X")
+    move(board, index, "X")
     display_board(board)
   else
     turn(board)
